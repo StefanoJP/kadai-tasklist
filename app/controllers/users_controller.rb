590eae1f471885962caa71bff8023d56c83e5585
EC2 Default User <ec2-user@ip-172-31-13-104.us-east-2.compute.internal>
2018-02-21 14:35:49 +0000

    direct link-to toppage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,13 @@
 class UsersController < ApplicationController
-  
+  #showへのフィルター
+  before_action :set_user, only: [:show]
+  before_action :correct_user, only: [:show]
   
   def index
   end
 
   def show
-    @user = User.find(params[:id])
+    #@user = User.find(params[:id])
     @tasks = @user.tasks.order('created_at DESC').page(params[:page])
     counts(@user)
   end
@@ -18,6 +20,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      #ユーザー登録後、トップページへ自動アクセス
+      session[:user_id] = @user.id
       flash[:success] = 'ユーザを登録しました。'
       redirect_to root_url
     else
@@ -28,6 +32,16 @@ class UsersController < ApplicationController
   
   private
   
+  #showへのフィルター
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
+  #showへのフィルター
+  def correct_user
+    redirect_to root_url if @user != current_user
+  end
+
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
